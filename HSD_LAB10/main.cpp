31c7315a6b4c3bfe4b6eec3ea199d61b57cb1519
Yonghoon Park <jim6829@naver.com>
2018-06-08 12:36:21 +0900

    10

--- HSD_LAB10/main.cpp
+++ HSD_LAB10/main.cpp
@@ -9,8 +9,8 @@ int main(void)
 	// input vector size M
 	// output vector size N
 	// matix size M x N
-	int M = 32;
-	int N = 32;
+	int M = 100;
+	int N = 120;
 
 	float* flat = new float[M*(N+1)];
 	float* input = flat;
